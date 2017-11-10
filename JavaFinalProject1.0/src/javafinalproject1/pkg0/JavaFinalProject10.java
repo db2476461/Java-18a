1b1e334736cf69d4bafc90f02b5159341b749101
David B <dbobsb@hotmail.com>
2017-11-10 14:15:23 -0800

    Add files via upload

---
+++ JavaFinalProject1.0/src/javafinalproject1/pkg0/JavaFinalProject10.java
@@ -0,0 +1,34 @@
+/*
+Course: CIS-18A-Java
+Professor: John W O'Connor
+Group Name: The Phishermen
+Members: Malay Patel
+         Brandon Ramirez
+         David Branson
+         Jesse Garcia
+         Jiman Chung
+Final Project Description:
+
+It's time to put those programming skills to work! With a group of 3-5 of your classmates, you'll create a Java application of sufficient complexity.  
+You can choose what kind of program you'd like to build - if you're lost or don't have ideas, check out the programming challenges in the back of each chapter of your book.
+You'll need to have a minimum of the following:
+1. At least 3 classes / objects NOT including your "main" class.
+2. You'll need to read data from a file
+3. You'll need to write data to a file
+4. You'll need to accept input from the user. 
+5. Must also have a coding and styling guidelines. Entire group must follow the guidelines set by the group
+6. Each file must have a header to describe it.
+7. The program have to run (33% of grade). 
+8. Group evaluation worth 10% of grade. 
+ */
+package javafinalproject1.pkg0;
+
+public class JavaFinalProject10 {
+
+
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
